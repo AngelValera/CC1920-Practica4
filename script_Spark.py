74cbd74a33c664fbb8a047be7d6ca06a330ad7a1
AngelValera <angelvalera.epal@gmail.com>
2020-06-01 19:17:10 +0200

    version randomforrest modificado

--- script_Spark.py
+++ script_Spark.py
@@ -3,6 +3,7 @@
 # Autor: Ángel Valera Motos
 ###################################################################################################
 import sys
+import os.path
 from pyspark import SparkContext, SparkConf, SQLContext
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
@@ -76,28 +77,32 @@ def prepocesamiento(dataFrame):
 
 # Función para clasificar usando RandomForest 
 #-------------------------------------------------------------------------------
-def clasificador_RandomForest(dataFrame):
+def clasificador_RandomForest(dataFrame,numArboles):
+    # Ajustamos todo el conjunto de datos para incluir todas las etiquetas en el índice.
     labelIndexer = StringIndexer(
         inputCol='label', outputCol='indexedLabel').fit(dataFrame)
+    # Se identifican automáticamente las características categóricas e indexelas.
+    # Establecemos el maxCategories para que las características con> 4 valores distintos se traten como continuas.
     featureIndexer =\
         VectorIndexer(inputCol='features',
-                      outputCol='indexedFeatures', maxCategories=2).fit(dataFrame)
+                      outputCol='indexedFeatures', maxCategories=4).fit(dataFrame)
+    # Dividimos los datos en conjuntos de entrenamiento y prueba (30% retenido para la prueba)
     (trainingData, testData) = dataFrame.randomSplit([0.7, 0.3])
+    # Entrena un modelo RandomForest.
     rf = RandomForestClassifier(
-        labelCol='indexedLabel', featuresCol='indexedFeatures', numTrees=10)
+        labelCol='indexedLabel', featuresCol='indexedFeatures', numTrees=numArboles)
+    # Convertir etiquetas indexadas de nuevo a etiquetas originales.
     labelConverter = IndexToString(inputCol='prediction', outputCol='predictedLabel',
-                                   labels=labelIndexer.labels)
-    # Chain indexers and forest in a Pipeline
+                                   labels=labelIndexer.labels)    
     pipeline = Pipeline(
         stages=[labelIndexer, featureIndexer, rf, labelConverter])
-    # Train model.  This also runs the indexers.
+    # Modelo de entrenamiento
     model = pipeline.fit(trainingData)
-    # Make predictions.
+    # Hacer predicciones
     predictions = model.transform(testData)
-
-    # Select example rows to display.
+    # Seleccione filas de ejemplo para mostrar.
     predictions.select('predictedLabel', 'label', 'features').show(5)
-    # Select (prediction, true label) and compute test error
+    # Seleccionamos (prediction, true label) y calculamos el test error
     evaluator = MulticlassClassificationEvaluator(
         labelCol='indexedLabel', predictionCol='prediction', metricName='accuracy')
     accuracy = evaluator.evaluate(predictions)
@@ -119,11 +124,23 @@ def clasificador_RandomForest(dataFrame):
 if __name__ == '__main__':
     # Conectamos con Spark    
     sc = iniciar_Spark()    
-    # Comprobamos si existe el dataframe y si no existe lo generamos
-    df = cargarDatos(sc)
-    df = generarNuevoDF(df)
-    df = prepocesamiento(df)
-    
-    clasificador_RandomForest(df)
+    # Comprobamos si existe el dataframe y si no existe lo generamos    
+    fichero = '/user/ccsa14274858/filteredC.small.training'
+    fs = sc._jvm.org.apache.hadoop.fs.FileSystem.get(sc._jsc.hadoopConfiguration())
+    if fs.exists(sc._jvm.org.apache.hadoop.fs.Path(fichero)):
+        sqlc = SQLContext(sc)
+        df = sqlc.read.csv(fichero, header=True, inferSchema=True)
+    else:       
+        # Unimos cabecera y datos en un conjunto de datos 
+        df = cargarDatos(sc)
+        # Obtenemos un nuevo conjunto de datos con las columnas designadas
+        df = generarNuevoDF(df)
+    # Realizamos un prepocesamiento a los datos y los balanceamos con undersampling
+    df = prepocesamiento(df)    
+    # Realizamos una clasificación usando RandomForest     
+    clasificador_RandomForest(df, 10)
+    #clasificador_RandomForest(df, 75)
+    #clasificador_RandomForest(df, 150)
+
 
     sc.stop()
